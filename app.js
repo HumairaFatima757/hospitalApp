70c01c2dbbc7389849789579518277f1ecb99bca
HumairaFatima757 <fatimahumaira757@gmail.com>
2023-08-03 00:52:12 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+function getme(){
+    // let e = document.getElementById("opt1").value;
+    // let f = document.getElementById("opt2").value;
+    // let g = document.getElementById("opt3").value;
+    // let h = document.getElementById("opt4").value;
+    // let i = document.getElementById("opt5").value;
+    // let j = document.getElementById("opt6").value;
+    // let k = document.getElementById("opt7").value;
+    // console.log(e);
+    // let e = document.getElementById("opt1").value;
+
+    let e =  document.getElementById("disease").value;
+    console.log(e);
+    if(e === "cough"){
+      let g =   " use syrup:  cufex  "
+   let f = document.getElementById("inp1")
+   .innerHTML = g;
+
+}
+  else if(e === "fever"){
+    let h =   " use medicine: Acetaminophen  and ibuprofen  "
+   let i = document.getElementById("inp1")
+   .innerHTML = h;}
+  else if(e === "flu"){
+    let h =   " use medicine: panadol and codral  "
+   let i = document.getElementById("inp1")
+   .innerHTML = h;}
+  else if(e === "headache"){
+    let h =   " use medicine: crocin and panadol   "
+   let i = document.getElementById("inp1")
+   .innerHTML = h;}
+  else if(e === "backache"){
+    let h =   " use medicine: Artifen and use movax cream  "
+   let i = document.getElementById("inp1")
+   .innerHTML = h;}
+  else if(e === "dizziness"){
+    let h =   " use medicine: gravinate and serc  "
+   let i = document.getElementById("inp1")
+   .innerHTML = h;}
+  else {
+    let h =   " use medicine: cure for mumps   "
+   let i = document.getElementById("inp1")
+   .innerHTML = h;}
+//    let h =   " use medicine: Acetaminophen and ibuprofen  "
+//    let i = document.getElementById("opt2")
+//    .innerHTML = h
+//    console.log(i);
+
+  
+//    }
+  
+   
+let q = document.getElementById("inp1").style.visibility= "visible"; 
+// let p = document.getElementById("inp1").style.cssText=' v;font-size:30px;color:AQUA;left:80px;top:10px;';
+
+
+
+
+
+// document.getElementById("inp1").style.visibility= "visible";
+
+//     // console.log(f);
+//     // alert(f)
+
+
+}
